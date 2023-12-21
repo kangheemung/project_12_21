27de10b1225e283fe4f2a66414eb773d8e2e1d4c
kurotamashi30 <kurotamashi30@gmail.com>
2023-12-21 22:57:43 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState} from "react";
+import Axios from "axios";
 
 function App() {
+  const [name, setName] = useState("");
+  const [predictedAge, setPredictedAge] = useState(null);
+  const [characterData, setCharacterData] = useState({});
+
+
+  const fetchData = () => {
+      Axios.get(`https://swapi.dev/api/people/?search=${name}`)
+      .then((res) => {
+        const data = res.data.results;
+        if (data) {
+          setCharacterData({
+            name: data.name,
+            birthYear: data.birth_year
+          });
+        } else {
+          // Handle the situation where the search returns no results
+          alert('No character found!');
+          setCharacterData({});
+        }
+      })
+      .catch((error) => {
+        // You can handle any errors here
+      console.error("Error fetching data:", error);
+      });
+    };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <input
+          placeholder= "EX.Kang..."
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <button onClick = {fetchData}>prodict Age</button>
+        <div>{fetchData}</div>
+          <button>Generate Cat Fact!</button>
+          <h1>Name:{predictedAge?.name}</h1>
+          <h1>Age:{predictedAge?.age}</h1>
+          <h1>color:{predictedAge?.color}</h1>
+          {characterData.name && <h1>Name: {characterData.name}</h1>}
+          {characterData.birthYear && <h1>Birth Year: {characterData.birthYear}</h1>}
     </div>
   );
 }
---
+++ src/H1.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function H1() {
+  return (
+    <div>
+      H1
+    </div>
+  )
+}
+
+export default H1
